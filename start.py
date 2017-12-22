b5fd745c0e85d2180080680079d43742ef8629d9
scottessner <scott.essner@gmail.com>
2017-12-22 16:42:21 -0600

    Testing machine specific startup

--- start.py
+++ start.py
@@ -2,19 +2,29 @@ from thespian.actors import ActorSystem
 import app
 import sys
 import platform
+import socket
 
-portnum = int(sys.argv[1])
+port_number = int(sys.argv[1])
 hostname = platform.node()
+host_address = socket.gethostname(hostname)
+convention_leader = sys.argv[2]
 
-capability_names = (sys.argv + [''])[2].split(',')
-capabilities = dict([('Admin Port', portnum),
-                     ('Convention Address.IPv4', ('', 1900)),
+capability_names = (sys.argv + [''])[3].split(',')
+capabilities = dict([('Admin Port', port_number),
+                     ('Convention Address.IPv4', (convention_leader, 1900)),
                      ('HostName', hostname),
                     ] +
                     list(zip(capability_names, [True] * len(capability_names))))
 
 
 asys = ActorSystem('multiprocTCPBase', capabilities)
-controller = asys.createActor('app.MasterController',
-                           globalName='MasterController')
-asys.tell(controller, app.HandleRegistrationChanges(True))
+
+if host_address == convention_leader:
+    controller = asys.createActor('app.MasterController',
+                                  globalName='MasterController')
+    asys.tell(controller, app.HandleRegistrationChanges(True))
+    controller.createActor(actorClass='a0',
+                           targetActorRequirements={
+                               'HostName': hostname},
+                           globalName='NodeController'
+                           )
